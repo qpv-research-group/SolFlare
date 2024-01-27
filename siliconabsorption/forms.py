5bde12d2439331170d0708903392fbdc2c3c968d
Ned <nekins@unsw.edu.au>
2024-01-28 10:25:26 +1100

    Realised that required=False is necessary for all boolean entries in Django forms, otherwise the form does not submit unless the checkbox is clicked...

--- siliconabsorption/forms.py
+++ siliconabsorption/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class inputParameters(forms.Form):
     arc_thickness = forms.FloatField(label='ARC thicness (au)', initial=80)
-    texture = forms.BooleanField(label='Texture?', initial=False)
-    alrear = forms.BooleanField(label='Al rear?', initial=False)
+    texture = forms.BooleanField(label='Texture?', initial=False,required=False)
+    alrear = forms.BooleanField(label='Al rear?', initial=False,required=False)
 
