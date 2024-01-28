ab8afdb42225b2ece4d8dad2915049b8f6238cb3
Ned <nekins@unsw.edu.au>
2024-01-28 19:26:07 +1100

    Adding coherency list to the raytrace with TMM code.

--- siliconabsorption/models.py
+++ siliconabsorption/models.py
@@ -87,13 +87,14 @@ class siliconCalculator:
             else:   # In the case of an ARC
                 front_texture_ARC = regular_pyramids(elevation_angle=55, upright=True,
                                              interface_layers=[Layer(self.ARC_width, SiN)])
+                options.coherency_list = ['c', 'i']
                 if self.alrear == False: # In the case of no Al rear reflector
                     structure = rt_structure(textures=[front_texture_ARC, rear_texture],
                                              materials=[Si],
                                              widths=[Si_width],
                                              incidence=Air,
                                              transmission=Air,
-                                         #    use_TMM=True,
+                                             use_TMM=True,
                                              options=options)
                 else:
                     structure = rt_structure(textures=[front_texture_ARC, rear_texture],
