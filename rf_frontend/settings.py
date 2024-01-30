c873f59bdd7b8961d2ac1b8703fd99a841360870
Ned <nekins@unsw.edu.au>
2024-01-30 20:29:20 +1100

    Removed unused crispy forms references.

--- rf_frontend/settings.py
+++ rf_frontend/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap4',
     'siliconabsorption.apps.SiliconabsorptionConfig',
 ]
 
