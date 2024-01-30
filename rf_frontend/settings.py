684b861365c03c6481d3788f35a62cc4f83feb2b
Ned <nekins@unsw.edu.au>
2024-01-30 20:25:30 +1100

    Improved visual layout

--- rf_frontend/settings.py
+++ rf_frontend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'siliconabsorption.apps.SiliconabsorptionConfig',
 ]
 
@@ -124,3 +126,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- siliconabsorption/forms.py
+++ siliconabsorption/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+
 
 class inputParameters(forms.Form):
     silicon_thickness = forms.FloatField(label='Si thickness (um)', initial=180)
@@ -6,4 +9,6 @@ class inputParameters(forms.Form):
     arc_thickness = forms.FloatField(label='ARC thickness (nm)', initial=80)
     texture = forms.BooleanField(label='Texture?', initial=False,required=False)
     alrear = forms.BooleanField(label='Al rear?', initial=False,required=False)
+
+
 
