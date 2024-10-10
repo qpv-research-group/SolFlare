41d8da747bdfda9301b2d773a9cef22ba6037105
Phoebe Pearce <p.pearce@unsw.edu.au>
2024-10-10 11:43:42 +1100

    sliders for multiple junctions

--- efficiency_limit/forms.py
+++ efficiency_limit/forms.py
@@ -1,4 +1,31 @@
 from django import forms
 
+
 class EgForm(forms.Form):
-    eg_value = forms.FloatField(label='Eg Value', min_value=0, max_value=10)
+    num_junctions = forms.IntegerField(
+        label='Number of Junctions',
+        min_value=1,
+        max_value=10,
+        initial=1,
+        widget=forms.NumberInput(attrs={'id': 'num_junctions'})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Dynamically add fields for bandgaps
+        num_junctions = self.data.get('num_junctions', 1)
+        try:
+            num_junctions = int(num_junctions)
+        except ValueError:
+            num_junctions = 1
+
+        for i in range(1, num_junctions + 1):
+            field_name = f'eg_value_{i}'
+            self.fields[field_name] = forms.FloatField(
+                label=f'Eg Value {i}',
+                min_value=0,
+                max_value=10,
+                initial=1.0,
+                widget=forms.NumberInput(attrs={'class': 'eg-slider', 'id': f'eg_value_{i}'})
+            )
--- efficiency_limit/views.py
+++ efficiency_limit/views.py
@@ -1,5 +1,4 @@
 import numpy as np
-import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,15 +7,13 @@ from solcore.solar_cell import SolarCell
 from solcore.solar_cell_solver import solar_cell_solver
 from solcore.structure import Junction
 
-
 @csrf_exempt
 def calculate_efficiency(request):
     # Generate solar spectrum data
-    wl = np.linspace(280, 4400, 200) * 1e-9  # Reduced number of points for performance
+    wl = np.linspace(280, 4400, 200) * 1e-9
     light = LightSource(source_type='standard', version='AM1.5g', x=1240 / (wl * 1e9),
                         output_units='photon_flux_per_ev')
 
-    # Convert wavelength to energy in eV
     energy_ev = 1240 / (wl * 1e9)
     energy_ev = energy_ev[::-1]
 
@@ -25,11 +22,8 @@ def calculate_efficiency(request):
     spectrum_data = [{'x': float(e), 'y': float(i)} for e, i in zip(energy_ev, spectrum)]
 
     if request.method == 'POST':
-        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-            data = json.loads(request.body)
-            eg_value = float(data.get('eg_value', 1.0))
-        else:
-            eg_value = float(request.POST.get('eg_value', 1.0))
+        num_junctions = int(request.POST.get('num_junctions', 1))
+        eg_values = [float(request.POST.get(f'eg_value_{i}', 1.0)) for i in range(1, num_junctions + 1)]
 
         # Your efficiency calculation code here
         back_reflector = True
@@ -39,24 +33,25 @@ def calculate_efficiency(request):
 
         junction_list = []
 
-        for eg in [eg_value]:
-            junction_list.append(Junction(kind='DB', T=T, Eg=eg_value, A=1, R_shunt=np.inf, n=ideality))
+        for eg in eg_values:
+            junction_list.append(Junction(kind='DB', T=T, Eg=eg, A=1, R_shunt=np.inf, n=ideality))
 
         my_solar_cell = SolarCell(junction_list, T=T, R_series=0)
 
         solar_cell_solver(my_solar_cell, 'iv',
                           user_options={'T_ambient': T, 'db_mode': 'top_hat', 'voltages': V,
                                         'light_iv': True,
-                                        'internal_voltages': np.arange(-1, np.sum([eg_value]) + 0.8,
-                                                                       0.01),
+                                        'internal_voltages': np.arange(-1, np.sum(eg_values) + 0.8, 0.01),
                                         'wavelength': wl,
                                         'mpp': True, 'light_source': light})
 
         # Get efficiency values
         efficiency = np.round(my_solar_cell.iv.Eta * 100, 2)
         voc = np.round(my_solar_cell.iv.Voc, 3)
-        jsc = np.round(my_solar_cell.iv.Isc, 1)
+        jsc = np.round(my_solar_cell.iv.Isc/10, 1)
         ff = np.round(my_solar_cell.iv.FF * 100, 2)
+
+        jsc_per_junction = [-my_solar_cell[i1].iv(0)/10 for i1 in range(len(eg_values))]
 
         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
             return JsonResponse({
@@ -64,6 +59,7 @@ def calculate_efficiency(request):
                 'voc': voc,
                 'jsc': jsc,
                 'ff': ff,
+                'jsc_per_junction': jsc_per_junction,
             })
 
     return render(request, 'calculate.html', {'spectrum_data': spectrum_data})
